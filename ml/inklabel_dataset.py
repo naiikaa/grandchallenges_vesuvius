b5c28dd2fb099555c5c8d8f90b31c376c8d4e425
naikaida <np@uni-kassel.de>
2025-06-30 15:07:24 +0200

    reworked the dataset so it now load labels and pergament parts

--- ml/inklabel_dataset.py
+++ ml/inklabel_dataset.py
@@ -5,6 +5,7 @@ import multiprocessing as mp
 from tqdm import tqdm
 from vesuvius import Volume
 import numpy as np
+import matplotlib.pyplot as plt
 
 class InkLabelDataset(torch.utils.data.Dataset):
     def __init__(self, segment_ids,sample_size=1024):
@@ -12,16 +13,19 @@ class InkLabelDataset(torch.utils.data.Dataset):
         assert len(segment_ids) > 0, f"segment_ids list cannot be empty has length {len(segment_ids)}"
 
         self.sample_size = sample_size
+        self.sample_count = 0
         self.segment_ids = segment_ids
-        print(f"Creating InkLabelDataset with {len(segment_ids)} segments...")
-        self.ink_labels = self.get_ink_labels(self.segment_ids)
-        print(f"Found {len(self.ink_labels)} ink labels for {len(segment_ids)} provided segments.")
         
-        print(f"Creating samples from ink labels...")
-        self.samples = self.create_samples_from_ink_labels(self.ink_labels)
-        self.samples = [torch.tensor(sample, dtype=torch.float32) for sample in self.samples]
-        self.samples = torch.concat(self.samples, dim=0)
-        print(f"Created {len(self.samples.shape)}")
+        print(f"Creating InkLabelDataset with {len(segment_ids)} segments...")
+        
+        self.data = self.get_data(self.segment_ids)
+        print(f"Found {len(self.data)} data points for {len(segment_ids)} provided segments.")
+        
+
+        
+        self.samples = {}
+        self.samples = self.create_samples_from_data()[0]
+        
         print(f"Created {len(self.samples)} samples from ink labels.")
         
 
@@ -30,48 +34,75 @@ class InkLabelDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
     
-        return self.samples[idx]
+        return self.samples[f'{idx}']
     
-    def get_ink_labels(self, segment_ids):
+    def get_data(self, segment_ids):
         # pull labels in parallel
         with mp.Pool(mp.cpu_count()) as pool:
-            results = list(pool.map(self.get_inklabels_from_id, segment_ids))
+            data = list(pool.map(self.get_data_from_id, segment_ids))
         
-        return [result for result in results if result is not None]
+        for item in data:
+            if item is None:
+                data.remove(item)
         
-    def get_inklabels_from_id(self, segment_id):
+        return data
+        
+    def get_data_from_id(self, segment_id):
         try:
             volume = Volume(int(segment_id))
             inklabels = volume.inklabel
+            pergament = volume[26:42,:,:]
         except Exception as e:
             print(f"Error processing segment {segment_id}: {e}")
+            self.segment_ids.remove(segment_id)
             return None
         
         if len(inklabels.shape) > 2:
             # If the inklabels are 3D, we take the first slice
             inklabels = inklabels[:,:,0]
         
-        return inklabels
+        
+        return {'inklabel': inklabels, 'scrollsegment': pergament, 'segment_id': segment_id}
 
-    def create_samples_from_ink_labels(self, ink_labels):
+    def create_samples_from_data(self):
         with mp.Pool(mp.cpu_count()) as pool:
-            samples = list(pool.map(self.create_sample_from_ink_label, ink_labels))
-            # downsample the samples
-            
+            samples = pool.map(self.create_sample_from_data, self.data)
+        
         return samples
+        
             
 
     
-    def create_sample_from_ink_label(self, ink_label):
-        samples = []
+    def create_sample_from_data(self, item):
+        samples = {}
+        ink_label = item['inklabel']
+        pergament = item['scrollsegment']
         h, w = ink_label.shape
+        
         for i in range(0, h - self.sample_size + 1, self.sample_size):
             for j in range(0, w - self.sample_size + 1, self.sample_size):
-                sample = ink_label[i:i + self.sample_size, j:j + self.sample_size]
-                if sample.max() > 0 and sample.shape[0] == self.sample_size and sample.shape[1] == self.sample_size:
-                    samples.append(sample)
-        
-        print(f"Created {len(samples)} samples from ink label with shape {ink_label.shape}")
+                ink_label_sample = ink_label[i:i + self.sample_size, j:j + self.sample_size]
+                pergament_sample = pergament[:,i:i + self.sample_size, j:j + self.sample_size]
+                if ink_label_sample.max() > 0 and ink_label_sample.shape[0] == self.sample_size and ink_label_sample.shape[1] == self.sample_size:
+                    samples[f'{self.sample_count}'] = {'ink_label': torch.tensor(np.array(ink_label_sample)), 'scroll_segment': torch.tensor(np.array(pergament_sample)), 'segment_id': item['segment_id']}
+                    self.sample_count += 1
+                    
+        print(f"Created {len(samples)} samples from ink label with shapes {ink_label.shape}, {pergament.shape} for segment {item['segment_id']}")
         
         return samples
-        
+    
+    def __test__(self):
+        # Test the dataset
+        print(f"Dataset length: {len(self)}")
+        fig , axes = plt.subplots(2, 5, figsize=(10, 25))
+        for i in range(5):
+            sample = self[i]
+            print(f"Sample {i}: ink_label shape {sample['ink_label'].shape}, scroll_segment shape {sample['scroll_segment'].shape}")
+            print(f"Sample {i} ink_label max value: {sample['ink_label'].max()}")
+            print(f"Sample {i} scroll_segment max value: {sample['scroll_segment'].max()}")
+            axes[0, i].imshow(sample['ink_label'].numpy(), cmap='gray')
+            axes[1, i].imshow(sample['scroll_segment'].numpy()[0,:,:], cmap='gray')
+            axes[0, i].axis('off')
+            axes[1, i].axis('off')
+        plt.tight_layout()
+        plt.show()
